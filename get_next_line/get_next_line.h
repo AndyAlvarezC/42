55c9761db83d456ed715a398c3bb22bc937d2ddc
Andy Alvarez <103003582+AndyAlvarezC@users.noreply.github.com>
2023-12-27 00:22:30 +0100

    Revisar errores

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andalvar <andalvar@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/28 14:10:32 by andalvar          #+#    #+#             */
+/*   Updated: 2023/11/28 14:10:32 by andalvar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <limits.h>
+# include <stdarg.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <limits.h>
+
+size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char *s1);
+char	*ft_strchr(char *s, int c);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*get_next_line(int fd);
+char	*ft_free(char **str);
+
+#endif
